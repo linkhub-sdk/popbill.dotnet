fa7f1db7ae4c379a3e9b74b06da2f1189d2a6c15
김성준 <pallet027@gmail.com>
2014-10-23 15:36:43 +0900

    Add MessageService

---
+++ Message/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Popbill.Message
+{
+    [DataContract]
+    public class Message
+    {
+        [DataMember(Name="snd")]
+        public string sendNum;
+        [DataMember(Name="rcv")]
+        public string receiveNum;
+        [DataMember(Name="rcvnm")]
+        public string receiveName;
+        [DataMember(Name="sjt")]
+        public string subject;
+        [DataMember(Name="msg")]
+        public string content;
+    }
+}
---
+++ Message/MessageResult.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Popbill.Message
+{
+    [DataContract]
+    public class MessageResult
+    {
+        [DataMember(Name="state")]
+        private int? _state = null;
+        [DataMember(Name = "subject")]
+        private string _subject = null;
+        [DataMember(Name = "type")]
+        private string _type = null;
+        [DataMember(Name = "content")]
+        private string _content = null;
+        [DataMember(Name = "sendNum")]
+        private string _sendNum = null;
+        [DataMember(Name = "receiveNum")]
+        private string _receiveNum = null;
+        [DataMember(Name = "receiveName")]
+        private string _receiveName = null;
+        [DataMember(Name = "reserveDT")]
+        private string _reserveDT = null;
+        [DataMember(Name = "sendDT")]
+        private string _sendDT = null;
+        [DataMember(Name = "resultDT")]
+        private string _resultDT = null;
+        [DataMember(Name = "sendResult")]
+        private string _sendResult = null;
+
+        public int? state { get { return _state; } }
+        public string subject { get { return _subject; } }
+        public MessageType type { get { return (MessageType)Enum.Parse(typeof(MessageType),_type); } }
+        public string content { get { return _content; } }
+        public string sendNum { get { return _sendNum; } }
+        public string receiveNum { get { return _receiveNum; } }
+        public string receiveName { get { return _receiveName; } }
+        public string reserveDT { get { return _reserveDT; } }
+        public string sendDT { get { return _sendDT; } }
+        public string resultDT { get { return _resultDT; } }
+        public string sendResult { get { return _sendResult; } }
+
+    }
+}
