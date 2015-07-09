dacb9c74a69b5e1e833e28a53b433cb480a925ed
JohnJeong123 <frenchofkiss@gmail.com>
2015-07-09 11:25:54 +0900

    Add Closedown SDK

---
+++ Closedown/ClosedownService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Popbill;
+
+namespace Popbill.Closedown
+{
+    public class ClosedownService : BaseService
+    {
+        public ClosedownService(String LinkID, String SecretKey)
+            : base(LinkID, SecretKey)
+        {
+            this.AddScope("170");
+        }
+
+        public Single GetUnitCost(String CorpNum)
+        {
+            UnitCostResponse response = httpget<UnitCostResponse>("/CloseDown/UnitCost", CorpNum, null);
+
+            return response.unitCost;
+        }
+
+        public CorpState checkCorpNum(String MemberCorpNum, String CheckCorpNum)
+        {
+            if (CheckCorpNum == null || CheckCorpNum == "")
+            {
+                throw new PopbillException(-99999999, "조회할 사업자번호가 입력되지 않았습니다");
+            }
+
+            return httpget<CorpState>("/CloseDown?CN=" + CheckCorpNum, MemberCorpNum, null);
+        }
+
+        public List<CorpState> checkCorpNums(String MemberCorpNum, List<String> CorpNumList)
+        {
+            if (CorpNumList == null || CorpNumList.Count == 0) throw new PopbillException(-99999999, "조회할 사업자번호 목록이 입력되지 않았습니다.");
+
+            String PostData = toJsonString(CorpNumList);
+
+            return httppost<List<CorpState>>("/CloseDown", MemberCorpNum, null, PostData, null);
+        }
+    }
+}
---
+++ Closedown/CorpState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Popbill.Closedown
+{
+    [DataContract]
+    public class CorpState
+    {
+        [DataMember]
+        public String corpNum;
+        [DataMember]
+        public String type;
+        [DataMember]
+        public String state;
+        [DataMember]
+        public String stateDate;
+        [DataMember]
+        public String checkDate;
+    }
+}
