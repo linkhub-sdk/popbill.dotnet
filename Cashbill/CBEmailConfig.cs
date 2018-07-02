483a88c69c9ddc4b318aef438da0c1e5e60bab75
kimhyunjin <khjzzm@gmail.com>
2018-07-02 14:40:29 +0900

    EmailConfig API added

---
+++ Cashbill/CBEmailConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Popbill.Cashbill
+{
+    [DataContract]
+    public class CBEmailConfig
+    {
+
+        [DataMember]
+        public String emailType;
+        [DataMember]
+        public bool? sendYN;
+    }
+}
---
+++ Taxinvoice/TIEmailConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Popbill.Taxinvoice
+{
+    [DataContract]
+    public class TIEmailConfig
+    {
+
+        [DataMember]
+        public String emailType;
+        [DataMember]
+        public bool? sendYN;
+    }
+}
