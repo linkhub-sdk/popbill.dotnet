c96fa0e8eeefcd89dfca1b4165f33f8c161d97a5
JohnJeong123 <frenchofkiss@gmail.com>
2019-12-24 23:52:22 +0900

    added EasyFinService

---
+++ EasyFin/EasyFinBankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Runtime.Serialization;
+namespace Popbill.EasyFin
+{
+    public class EasyFinBankAccount
+    {
+        [DataMember]
+        public string accountNumber;
+
+        [DataMember]
+        public string bankCode;
+
+        [DataMember]
+        public string accountName;
+
+        [DataMember]
+        public string accountType;
+
+        [DataMember]
+        public int? state;
+
+        [DataMember]
+        public string regDT;
+
+        [DataMember]
+        public string memo;
+
+    }
+}
