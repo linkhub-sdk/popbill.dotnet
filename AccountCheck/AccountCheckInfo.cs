478676265ebf80e40e8d3361373042e1ab1fdbc7
JohnJeong123 <frenchofkiss@gmail.com>
2020-06-30 14:34:28 +0900

    added AccountCheck Service

---
+++ AccountCheck/AccountCheckInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Popbill.AccountCheck
+{
+    [DataContract]
+    public class AccountCheckInfo
+    {
+        [DataMember]
+        public String bankCode;
+        [DataMember]
+        public String accountNumber;
+        [DataMember]
+        public String accountName;
+        [DataMember]
+        public String checkDate;
+        [DataMember]
+        public String resultCode;
+        [DataMember]
+        public String resultMessage;
+    }
+}
